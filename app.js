ea602828f310131334ed46bf267f360cede8ec45
RehberOdhano <rehber.odhano30@gmail.com>
2022-04-21 01:52:28 +0500

    updated the routers and controllers files...

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { auth, requiresAuth } = require('express-openid-connect');
+const { auth } = require('express-openid-connect');
 const user_routes = require('./routes/user-route');
 
 const app = express();
