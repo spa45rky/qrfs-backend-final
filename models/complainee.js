785f42affb96bb9c196cdab4363af7ad03cfa91d
RehberOdhano <rehber.odhano30@gmail.com>
2022-04-21 00:43:13 +0500

    added complainee schema...

--- models/complainee.js
+++ models/complainee.js
@@ -2,15 +2,29 @@ const mongoose = require('mongoose');
 
 const complaineeSchema = mongoose.Schema({
     complaints: [{
-
+        title: String,
+        description: String,
+        category: String,
+        status: String,
+        assignedTo: [{
+            ID: { type: String },
+            username: { type: String },
+            email: { type: String },
+        }],
     }],
     feedback: [{
-
+        complaint_id: String,
+        response: String,
     }],
     rating: { type: Number },
     level: { type: Number },
     points: { type: Number },
-    inventory: {}
+    inventory: [{
+        videos: [{
+            data: Buffer,
+            contentType: String
+        }]
+    }]
 
 }, { versionKey: false });
 
