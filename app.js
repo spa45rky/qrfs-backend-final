61cdd374b3e4ca87657b97bf02e1284a09a564c3
RehberOdhano <rehber.odhano30@gmail.com>
2022-04-20 14:35:33 +0500

    added routers and controllers...

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { auth, requiresAuth } = require('express-openid-connect');
+const router = require('./router');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('/', router);
 
 app.use(
     auth({
