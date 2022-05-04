5e38ecafcfa80e456072ecbf6b8961c86ad1c011
RehberOdhano <rehber.odhano30@gmail.com>
2022-05-04 18:31:49 +0500

    issue is resolved!

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(auth({
-    authRequired: false, // otherwise every single route requires the authentication         
-    auth0Logout: true,
-    issuerBaseURL: process.env.ISSUER_BASE_URL,
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.CLIENT_ID,
-    secret: process.env.SECRET,
-    idpLogout: true,
-}));
+// app.use(auth({
+//     authRequired: false, // otherwise every single route requires the authentication         
+//     auth0Logout: true,
+//     issuerBaseURL: process.env.ISSUER_BASE_URL,
+//     baseURL: process.env.BASE_URL,
+//     clientID: process.env.CLIENT_ID,
+//     secret: process.env.SECRET,
+//     idpLogout: true,
+// }));
 
 app.use('/', auth_routes);
 
