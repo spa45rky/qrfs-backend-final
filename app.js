664e46283abd9c6d02f2bca0c28a1d6e0e97b41c
RehberOdhano <rehber.odhano30@gmail.com>
2022-05-04 18:25:49 +0500

    issue resolved!

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { auth } = require('express-openid-connect');
+const auth_routes = require('./routes/user-route');
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(auth({
+    authRequired: false, // otherwise every single route requires the authentication         
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+    idpLogout: true,
+}));
+
+app.use('/', auth_routes);
+
+module.exports = app;
