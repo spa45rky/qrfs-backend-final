abbe15d4f3abe24d9d5440f8bc5fa5d6f2c5a031
spa45rky <spa45rky@gmail.com>
2022-05-21 19:46:24 +0500

    leaving control

--- controllers/superadminController.js
+++ controllers/superadminController.js
@@ -6,7 +6,7 @@ exports.addCustomer = async (req, res) => {
     try {
         let salt = bcrypt.genSaltSync(10);
         await Customer.findOne( {email: req.body.email}, async (err, customer) => {
-            if (err) console.log(err);
+            if (err) {console.log(err);}
             else if (customer) res.send('CUSTOMER ALREADY EXISTS!');
             else {
                 await Customer.create({
@@ -15,7 +15,7 @@ exports.addCustomer = async (req, res) => {
                 }, async (err, customer) => {
                     if (err) {
                         console.log("\n\n" + err);
-                        res.send("UNABLE TO ADD CUSTOMER!");
+                        res.send(err)
                     }
                     await User.create({
                         name: 'UNDEFINED',
@@ -24,13 +24,17 @@ exports.addCustomer = async (req, res) => {
                         role: 'ADMIN',
                         sign_type: 'PLATFORM',
                         company_id: customer._id
-                    }, (err, user) => {
+                    }, async (err, user) => {
                         if (err) res.send('CUSTOMER CREATED BUT ADMIN ACCOUNT NOT CREATED!')
-                        res.json(customer)
+                        customer.employees
+                        await Customer.findByIdAndUpdate(customer._id, { $push: { customer_id: user._id } }, (err, finalCustomer) => {
+                            if (err) res.send(err)
+                            res.json(customer)
+                        })
                     })
                 })
             }
-        } )
+        }).clone();
     } catch (error) {
         res.send(error)
     }
