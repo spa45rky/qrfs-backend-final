ffa981af1b82be45b9b4227536d2755cc1051c44
spa45rky <spa45rky@gmail.com>
2022-05-21 23:16:24 +0500

    added sockets

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const user_routes = require('./routes/user-routes');
 const service_provider_routes = require('./routes/service-provider-routes');
 const auth = require('./middlewares/auth');
 const passport = require('passport');
+const http = require('http');
+const { Server } = require('socket.io');
+
 
 const app = express();
-
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -22,5 +24,29 @@ app.use('/superadmin', superadmin_routes)
 // app.use('/admin', admin_routes);
 app.use('/user', user_routes);
 app.use('/serviceprovider', service_provider_routes);
+
+
+// Sockets for real-time updates
+const server = http.createServer(app)
+
+// instantializing socket server
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("USER DISCONNECTED!", socket.id);
+    })
+})
+
+server.listen(3009, () => {
+    console.log("SERVER RUNNING!")
+})
 
 module.exports = app;
