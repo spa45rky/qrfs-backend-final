ebbe21864b37d3305382b7c5fa68c1ca06523356
RehberOdhano <rehber.odhano30@gmail.com>
2022-05-23 22:57:04 +0500

    fixed the issue from updateComplaint - sp-side route

--- routes/service-provider-routes.js
+++ routes/service-provider-routes.js
@@ -3,7 +3,7 @@ const service_provider_router = express.Router();
 const serviceProviderController = require('../controllers/serviceProviderController');
 
 service_provider_router.get('/complaints/assigned/:id', serviceProviderController.getAssignedComplaints);
-service_provider_router.put('/complaints/update/:sp_id/:c_id', serviceProviderController.updateComplaint);
+service_provider_router.put('/complaints/update/:id', serviceProviderController.updateComplaint);
 service_provider_router.put('/complaints/resolve/:sp_id/:c_id', serviceProviderController.resolveComplaint);
 service_provider_router.put('/complaints/transfer/:sp_id/:t_id/:c_id', serviceProviderController.transferComplaint);
 
